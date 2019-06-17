13e0fa2e476327368c6beda08e0d142639439be6
Mateusz <mateuszmaj@o2.pl>
2019-06-17 22:57:48 +0200

    Moq part2

--- UnitTests/MoqTest/Moq2.cs
+++ UnitTests/MoqTest/Moq2.cs
@@ -1,4 +1,5 @@
 ﻿using Moq;
+using Moq.Protected;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -60,6 +61,51 @@ namespace UnitTests.MoqTest
         string Name { get; set; }
         IBaz SomeBaz { get; }
         int SomeOtherProperty { get; set; }
+    }
+
+    public class Consumer
+    {
+        private IFoo foo;
+
+        public Consumer(IFoo foo)
+        {
+            this.foo = foo;
+        }
+
+        public void Hello()
+        {
+            foo.DoSomething("ping");
+            var name = foo.Name;
+            foo.SomeOtherProperty = 123;
+        }
+    }
+
+    public delegate void AlienAbductionEventHandler(int galaxy, bool returned);
+
+    public interface IAnimal
+    {
+        event EventHandler FallsIll;
+
+        void Stumble();
+
+        event AlienAbductionEventHandler AbductedByAliens;
+    }
+
+    public class Doctor
+    {
+        public int TimesCured;
+        public int AbductionObserved;
+
+        public Doctor(IAnimal animal)
+        {
+            animal.FallsIll += (sender, args) =>
+            {
+                Console.WriteLine("I will cure you!");
+                TimesCured++;
+            };
+
+            animal.AbductedByAliens += (galaxy, returned) => ++AbductionObserved;
+        }
     }
 
     [TestFixture]
@@ -251,6 +297,135 @@ namespace UnitTests.MoqTest
             mock.SetupAllProperties();
             foo.SomeOtherProperty = 123;
             Assert.That(mock.Object.SomeOtherProperty, Is.EqualTo(123));
+        }
+
+        [Test]
+        public void EventTest()
+        {
+            var mock = new Mock<IAnimal>();
+            var doctor = new Doctor(mock.Object);
+
+            mock.Raise(
+                a => a.FallsIll += null, // wskaz event dla ktorego trzeba mockowac
+                new EventArgs()
+                );
+
+            Assert.That(doctor.TimesCured, Is.EqualTo(1));
+
+            mock.Setup(a => a.Stumble()) // wiemy, ze kiedy sie potknie staje sie chore
+                .Raises(a => a.FallsIll += null,
+                new EventArgs());
+
+            mock.Object.Stumble();
+            Assert.That(doctor.TimesCured, Is.EqualTo(2));
+
+            mock.Raise(a => a.AbductedByAliens += null,
+                42, true);
+            Assert.That(doctor.AbductionObserved, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void CallbackTest()
+        {
+            var mock = new Mock<IFoo>();
+
+            int x = 0;
+            mock.Setup(foo => foo.DoSomething("ping"))
+                .Returns(true)
+                .Callback(() => x++);   // wykonaj po kazdym wykonaniu
+
+            mock.Object.DoSomething("ping");
+            Assert.That(x, Is.EqualTo(1));
+
+            // wykorzystaj argument metody w callbacku
+            mock.Setup(foo => foo.DoSomething(It.IsAny<string>()))
+                .Returns(true)
+                .Callback((string s) => x += s.Length);
+
+            // generycznie
+            mock.Setup(foo => foo.DoSomething(It.IsAny<string>()))
+                .Returns(true)
+                .Callback<string>(s => x += s.Length);
+
+            // callback przed wywolaniem metody
+            mock.Setup(foo => foo.DoSomething("pong"))
+                .Callback(() => Console.WriteLine("before pong"))
+                .Returns(false)
+                .Callback(() => Console.WriteLine("after pong"));
+
+            mock.Object.DoSomething("pong");
+        }
+
+        [Test]
+        public void VerificationTest()
+        {
+            var mock = new Mock<IFoo>();
+            var consumer = new Consumer(mock.Object);
+
+            consumer.Hello();
+
+            mock.Verify(foo => foo.DoSomething("ping"), Times.AtLeastOnce);
+
+            mock.Verify(foo => foo.DoSomething("pong"), Times.Never);
+
+            mock.VerifyGet(foo => foo.Name);
+
+            mock.VerifySet(foo => foo.SomeOtherProperty = It.IsInRange(100, 200, Range.Inclusive));
+
+
+        }
+
+        [Test]
+        public void MockBehaviour()
+        {
+            var mock = new Mock<IFoo>(MockBehavior.Strict); // strict - wyjatek jesli nieokreslone wywolanie
+
+            //mock.Object.DoSomething("abc"); // wyjatek
+
+            mock.Setup(f => f.DoSomething("abc"))
+                .Returns(true);
+
+            mock.Object.DoSomething("abc");
+        }
+
+        [Test]
+        public void RecursiveMock()
+        {
+            var mock = new Mock<IFoo>
+            {
+                DefaultValue = DefaultValue.Mock
+            };
+
+            var baz = mock.Object.SomeBaz;
+            var bazMock = Mock.Get(baz);
+
+            bazMock.SetupGet(f => f.Name).Returns("abc");
+
+            var mockRepository = new MockRepository(MockBehavior.Strict)
+            {
+                DefaultValue = DefaultValue.Mock
+            };
+
+            var fooMock = mockRepository.Create<IFoo>();
+            var otherMock = mockRepository.Create<IBaz>(MockBehavior.Loose);
+
+            mockRepository.Verify();
+        }
+        
+
+        public abstract class Person
+        {
+            protected int SSN { get; set; }
+            protected abstract void Execute(string cmd);
+        }
+        [Test]
+        public void ProtectedMembers()
+        {
+            var mock = new Mock<Person>();
+
+            mock.Protected().SetupGet<int>("SSN").Returns(42);
+
+            mock.Protected().Setup<string>("Execute", ItExpr.IsAny<string>());
         }
     }
 }
